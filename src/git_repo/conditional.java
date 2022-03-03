c75b169db459294abadec548a7e36be7af73e1a9
prabhat997 <prabhat.khanduri67@gmail.com>
2022-03-03 11:29:24 +0530

    first java programme

---
+++ src/git_repo/conditional.java
@@ -0,0 +1,25 @@
+package git_repo;
+
+import java.util.Scanner;
+
+public class conditional {
+	
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+  //      System.out.println("Enter the number:");
+        int N = sc.nextInt();
+        if(N % 2 == 0){
+	        if((N >=2 && N <= 5) || (N > 20)){
+	            System.out.println("Not Weird");
+	        }
+	        else if(N >= 6 && N <= 20){
+	            System.out.println("Weird");
+	        }   
+         }
+        else{
+         System.out.println("Weird");
+    }
+
+    }
+}
